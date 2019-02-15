a8922670b05755f2dcedead4125fbba78cadf689
Putova Yulia <33568029+yuliapu@users.noreply.github.com>
2019-02-15 18:26:27 +0200

    Update Main.java

--- src/com/qawinter/Main.java
+++ src/com/qawinter/Main.java
@@ -7,10 +7,10 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.print("\t\t * * * *   * * * *       *        *     * \n" +
-                "\t\t *         *     *     *   *      * * * * \n" +
-                "\t\t * *       * * * *    *     *     *  *  * \n" +
-                "\t\t *         *         * * * * *    *     * \n" +
-                "\t\t * * * *   *        *         *   *     * ");
+                         "\t\t *         *     *     *   *      * * * * \n" +
+                         "\t\t * *       * * * *    *     *     *  *  * \n" +
+                         "\t\t *         *         * * * * *    *     * \n" +
+                         "\t\t * * * *   *        *         *   *     * ");
 
     }
 }
