0da8487763b4dc3f1b8b190b1c3920ff4758ed81
yuliapu <iamsherlocked9918@gmail.com>
2019-02-15 18:25:41 +0200

    Home task 1 by Putova Yulia.

---
+++ src/com/qawinter/Main.java
@@ -0,0 +1,16 @@
+package com.qawinter;
+/**
+ * Created by Putova Yulia.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.print("\t\t * * * *   * * * *       *        *     * \n" +
+                "\t\t *         *     *     *   *      * * * * \n" +
+                "\t\t * *       * * * *    *     *     *  *  * \n" +
+                "\t\t *         *         * * * * *    *     * \n" +
+                "\t\t * * * *   *        *         *   *     * ");
+
+    }
+}
