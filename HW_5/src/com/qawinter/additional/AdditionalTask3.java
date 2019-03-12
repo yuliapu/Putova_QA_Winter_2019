a356ea67c8c2f778262ccf8f5972022b9326cefe
Putova Yulia <33568029+yuliapu@users.noreply.github.com>
2019-03-12 18:54:06 +0200

    Update AdditionalTask3.java

--- HW_5/src/com/qawinter/additional/AdditionalTask3.java
+++ HW_5/src/com/qawinter/additional/AdditionalTask3.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class AdditionalTask3 {
     public static void main(String[] args) {
-        System.out.println("Home work 5 by Putova Yulia. Additional task 2.");
+        System.out.println("Home work 5 by Putova Yulia. Additional task 3.");
         int[] array = createArray(inputSize());
         findMinMax(array);
 
